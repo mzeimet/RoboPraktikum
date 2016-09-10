8ad7d271a5f3faef8e6c6bba27e67dcb8ef89eaf
mzeimet <marvin.zeimet@aol.com>
2016-09-10 20:35:37 +0200

    hardstop rein, machePlatz raus

--- src/project/Robot.java
+++ src/project/Robot.java
@@ -102,8 +102,8 @@ public class Robot {
 					if(zielGefunden) return;
 				}
 			}
-
-			machePlatz();
+			motor.hardStop();
+//			machePlatz();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -113,10 +113,10 @@ public class Robot {
 	 * Macht Platz damit der dumme ihn nicht rammt
 	 */
 	private void machePlatz() {
-		motor.fahreGerade(1);
-		motor.setGeschwindigkeit(0);
-		motor.forward();
-		motor.stop();
+//		motor.fahreGerade(1);
+//		motor.setGeschwindigkeit(0);
+//		motor.forward();
+//		motor.stop();
 
 	}
 
@@ -339,7 +339,6 @@ public class Robot {
 	 */
 	public int getLichtInProzent() {
 		int wert = new Double(lichtSensor.getWert() * 100.0).intValue();
-		System.out.println(wert);
 		return wert;
 	}
 }
